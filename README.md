0f02bd30c2c9cb349a620fce964961f8c6bfcc8e
beggers <be@spiegelburg.net>
2019-01-26 02:11:30 +0100

    Add layer and split

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This repository contains simplic document processing libraries and services. The service implementation
 mostly uses GdPicture components.
+
+Services:
+
+* ADR
+* OCR/DCE
+* Pdf
+* Format converter
---
+++ src/Simplic.DocumentProcessing/PdfLayer/IPdfLayerService.cs
@@ -0,0 +1,17 @@
+﻿namespace Simplic.DocumentProcessing
+{
+    /// <summary>
+    /// Pdf layer service
+    /// </summary>
+    public interface IPdfLayerService
+    {
+        /// <summary>
+        /// Add a layer to a pdf and return it
+        /// </summary>
+        /// <param name="pdf">Pdf to add the layer to</param>
+        /// <param name="layer">Layer file as byte arrya</param>
+        /// <param name="mode">Add mode (first page, each page, ...)</param>
+        /// <returns>Pdf with layer</returns>
+        byte[] MergeLayer(byte[] pdf, byte[] layer, PdfLayerMode mode);
+    }
+}
