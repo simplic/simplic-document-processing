3c5ed5f03ba82505ecf16778b1add49d977075de
Benedikt Eggers <be@spiegelburg.net>
2020-12-10 09:05:12 +0100

    Add QR-Code reading

---
+++ src/Simplic.DocumentProcessing/Barcode/IQRCodeRecognitionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.DocumentProcessing
+{
+    /// <summary>
+    /// Service for reading qr-codes
+    /// </summary>
+    public interface IQRCodeRecognitionService
+    {
+        /// <summary>
+        /// Execute barcode recognition
+        /// </summary>
+        /// <param name="blob">Blob to test</param>
+        /// <param name="fileExtension">file extension</param>
+        /// <returns>List of detected barcodes</returns>
+        IList<BarcodeRecognitionResult> Process(byte[] blob, string fileExtension);
+    }
+}
