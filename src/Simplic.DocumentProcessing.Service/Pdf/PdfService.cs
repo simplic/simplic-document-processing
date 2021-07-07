326defe1e8e707d0fcedb7048320c7720b5c1f84
koesem <koesem@bettels.info>
2021-07-07 15:52:51 +0200

    Added a new method "AddEmptyPage()" to the PDF-service

--- src/Simplic.DocumentProcessing.Service/Pdf/PdfService.cs
+++ src/Simplic.DocumentProcessing.Service/Pdf/PdfService.cs
@@ -28,5 +28,31 @@ namespace Simplic.DocumentProcessing
                 }
             }
         }
+
+        /// <summary>
+        /// Adds an empty page.
+        /// </summary>
+        /// <param name="pdf">Pdf</param>
+        /// <param name="pageNumber">Index of the new page, starting with 1 which will insert an empty page as the first page</param>
+        /// <returns>The resulting Pdf-blob</returns>
+        public byte[] AddEmptyPage(byte[] pdf, int pageNumber)
+        {
+            var pdfToTiffService = CommonServiceLocator.ServiceLocator.Current.GetInstance<PdfToTiffService>();
+            using (var stream = new MemoryStream(pdf))
+            {
+                using (var pdfInstance = GdPictureHelper.GetPDFInstance())
+                {
+                    pdfInstance.LoadFromStream(stream);
+                    pdfInstance.SelectPage(1);
+                    float pageWidth = pdfInstance.GetPageWidth();
+                    float pageHeight = pdfInstance.GetPageHeight();
+
+                    pdfInstance.InsertPage(pageWidth, pageHeight, pageNumber);
+                    byte[] blob = pdfToTiffService.Convert(pdf);
+                    pdfInstance?.CloseDocument();
+                    return blob;
+                }
+            }
+        }
     }
 }
--- src/Simplic.DocumentProcessing/Pdf/IPdfService.cs
+++ src/Simplic.DocumentProcessing/Pdf/IPdfService.cs
@@ -11,5 +11,12 @@
         /// <param name="pdf">Pdf</param>
         /// <returns>Page count</returns>
         int GetPageCount(byte[] pdf);
+        /// <summary>
+        /// Adds an empty page.
+        /// </summary>
+        /// <param name="pdf">Pdf</param>
+        /// <param name="pageNumber">Index of the new page, starting with 1 which will insert an empty page as the first page</param>
+        /// <returns>The resulting Pdf-blob</returns>
+        byte[] AddEmptyPage(byte[] pdf, int pageNumber);
     }
 }
